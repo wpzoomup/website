06dd1e123591648191ebe12338dea8aa3b08a929
ShizumiYoshiaki <ni.muzihs@shizum.in>
2020-07-24 11:05:29 +0900

    :+1: リッチテキストのタイトルが省略されないように変更

---
+++ wp-content/plugins/wpzoomup-customize-styles/template-parts/loop/entry-summary/title/title.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ * @version 10.7.0
+ */
+
+use Framework\Helper;
+
+$template_args = [
+	'title_tag' => Helper::get_var( $_title_tag, 'h2' ),
+];
+
+$layout    = get_theme_mod( get_post_type() . '-entries-layout' );
+$title_tag = $template_args['title_tag'];
+?>
+
+<<?php echo esc_html( $title_tag ); ?> class="c-entry-summary__title">
+<?php the_title(); ?>
+</<?php echo esc_html( $title_tag ); ?>>
