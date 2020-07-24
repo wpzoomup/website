85678c397e1311d3bc4fc55fe6bdbeecf172ef34
ShizumiYoshiaki <ni.muzihs@shizum.in>
2020-07-24 11:04:33 +0900

    :+1: SnowMonkeyのテンプレート上書き処理をプラグイン内のディレクトリで可能になるように設定

--- wp-content/plugins/wpzoomup-customize-styles/include/classes/class-wpzoomup-customize-style-base.php
+++ wp-content/plugins/wpzoomup-customize-styles/include/classes/class-wpzoomup-customize-style-base.php
@@ -13,7 +13,21 @@ class WPZoomUP_Customize_Style_Base {
 	 * WPZoomUP_Customize_Style_Base constructor.
 	 */
 	public function __construct() {
+		add_filter( 'snow_monkey_template_part_root_hierarchy', array( $this, 'custom_root_hierarchy' ) );
 	}
+
+	/**
+	 * SnowMonkeyのテンプレート上書きをこのプラグイン内で可能にする
+	 *
+	 * @param array $hierarchy
+	 * @return mixed
+	 */
+	public function custom_root_hierarchy( $hierarchy ) {
+		$hierarchy[] = WPZOOMUP_CUSTOMIZE_STYLES_PATH;
+
+		return $hierarchy;
+	}
+
 }
 
 new WPZoomUP_Customize_Style_Base();
