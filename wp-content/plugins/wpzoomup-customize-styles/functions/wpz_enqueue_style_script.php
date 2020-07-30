ff79273b6f772ca68fa7ebd098c9f4af9245b7ca
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2020-07-31 03:03:35 +0900

    Update wpz_enqueue_style_script.php

--- wp-content/plugins/wpzoomup-customize-styles/functions/wpz_enqueue_style_script.php
+++ wp-content/plugins/wpzoomup-customize-styles/functions/wpz_enqueue_style_script.php
@@ -3,9 +3,9 @@ add_action( 'wp_enqueue_scripts', 'wpz_enqueue_style_script' );
 function wpz_enqueue_style_script() {
 	wp_enqueue_style(
 		'wpz_style',
-		WPZOOMUP_CUSTOMIZE_STYLES_PATH . '/src/css/style.css',
+		WPZOOMUP_CUSTOMIZE_STYLES_URL . '/src/css/style.css',
 		[],
-		filemtime( WPZOOMUP_CUSTOMIZE_STYLES_PATH . '/src/css/style.css' )
+		filemtime( WPZOOMUP_CUSTOMIZE_STYLES_URL . '/src/css/style.css' )
 	);
 	/*
 	wp_enqueue_script(
