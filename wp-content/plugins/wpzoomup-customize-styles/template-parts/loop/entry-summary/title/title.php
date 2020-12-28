4b31d7a1c3b082a50c2449c7fe2734c14f7daf6c
lunaluna <web@lunalunadesign.com>
2020-12-28 20:39:42 +0900

    snow-monkey を 12.1.3 にバージョンアップした際のテンプレートのアップデートへの対応

--- wp-content/plugins/wpzoomup-customize-styles/template-parts/loop/entry-summary/title/title.php
+++ wp-content/plugins/wpzoomup-customize-styles/template-parts/loop/entry-summary/title/title.php
@@ -3,17 +3,23 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 10.7.0
+ * @version 11.6.0
  */
 
 use Framework\Helper;
 
-$template_args = [
-	'title_tag' => Helper::get_var( $_title_tag, 'h2' ),
-];
+$args = wp_parse_args(
+	// phpcs:disable VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable
+	$args,
+	// phpcs:enable
+	[
+		'_context'   => null,
+		'_title_tag' => 'h2',
+	]
+);
 
 $layout    = get_theme_mod( get_post_type() . '-entries-layout' );
-$title_tag = $template_args['title_tag'];
+$title_tag = $args['_title_tag'];
 ?>
 
 <<?php echo esc_html( $title_tag ); ?> class="c-entry-summary__title">
